358ad3291aa7ddd9352265afaecd4efcdde730dc
michaelwhyte <info@mwhyte.ca>
2017-01-19 15:21:03 -0800

    converted to ES6

--- scripts/script.js
+++ scripts/script.js
@@ -1,233 +1,237 @@
 // JavaScript Document
 
-// Main Game Object
-function MatchGame(gameBoard, boxes, numBoxes, turnsOut, matchedOut, btnPlay, picFilename){
+// Main Game Class
+class MatchGame {
 
-	// Game Properties
-
-	// Game HTML Elements
-	this.gameBoard = gameBoard;
-	this.boxes = boxes;
-	this.firstFlippedBox = null;
-	this.secondFlippedBox = null;
-	this.turnsOut = turnsOut;
-	this.matchedOut = matchedOut;
-	this.btnPlay = btnPlay;
-
-	// Game Variables
-	this.numBoxes = numBoxes;
-	this.halfNumBoxes = numBoxes / 2;
-	this.turns = 0;
-	this.matches = 0;
-	this.clickCounter = 0;
-	this.picFilename = picFilename;
-	this.pictures = [];
-	this.firstFlippedPicture = null;
-	this.secondFlippedPicture = null;
-
-}
-
-// Game Functions
-MatchGame.prototype.gameStart = function(){
-
-	this.btnPlay.text('Click to End Game');
-	this.createPictures();
-	this.shufflePictures();
-	this.addPictures(this.pictures);	
-	this.gameBoard.removeClass('no_game_board');
-
-};
-
-MatchGame.prototype.gameEnd = function(isGameWon){
-
-	this.gameBoard.addClass('no_game_board');
-	this.btnPlay.text('Click to Play Game');
-	
-	if(isGameWon === false){
+	// Game Constructor
+	constructor(gameBoard, boxes, numBoxes, turnsOut, matchedOut, btnPlay, picFilename){
 		
-		this.boxes.removeClass('flipped')
-	              .removeClass('matched');
-	
+		// Game HTML Elements
+		this.gameBoard = gameBoard;
+		this.boxes = boxes;
+		this.firstFlippedBox = null;
+		this.secondFlippedBox = null;
+		this.turnsOut = turnsOut;
+		this.matchedOut = matchedOut;
+		this.btnPlay = btnPlay;
+
+		// Game Variables
+		this.numBoxes = numBoxes;
+		this.halfNumBoxes = numBoxes / 2;
+		this.turns = 0;
+		this.matches = 0;
+		this.clickCounter = 0;
+		this.picFilename = picFilename;
+		this.pictures = [];
+		this.firstFlippedPicture = null;
+		this.secondFlippedPicture = null;
+		
 	}
 	
-	this.clickCounter = 0;
-	this.turns = 0;
-	this.matches = 0;
-	this.matchedOut.text(0);
-	this.turnsOut.text(0);
-	this.destroyPictures();
+	// Game Functions
+	gameStart(){
 
-};
+		this.btnPlay.text('Click to End Game');
+		this.createPictures();
+		this.shufflePictures();
+		this.addPictures(this.pictures);	
+		this.gameBoard.removeClass('no_game_board');
 
-MatchGame.prototype.createPictures = function(){
-
-	for(var i = 1; i <= this.halfNumBoxes; i++){
-		if(i < 10){			
-			// for images 01 - 09
-			// .push() inserts an image into the array..
-			this.pictures.push(this.picFilename + '0' + i + '.jpg');
-			this.pictures.push(this.picFilename + '0' + i + '.jpg');
-				
-		}else{
-			// for images 10 - 18
-			this.pictures.push(this.picFilename + i + '.jpg');
-			this.pictures.push(this.picFilename + i + '.jpg');
-			
-		} // end if (i < 10)	
-	} // end for loop
-
-}; // end createPictures
-
-MatchGame.prototype.shufflePictures = function(){
-
-	var counter = this.pictures.length;
-	var temp; 
-	var i;
-
-	// While there are elements in the array
-	while (counter > 0) {
-		
-		// Pick a random index
-		i = Math.floor(Math.random() * counter);
-
-		// Decrease counter by 1
-		counter--;
-
-		// And swap the last element with it
-		temp = this.pictures[counter];
-		this.pictures[counter] = this.pictures[i];
-		this.pictures[i] = temp;
+	} // end gameStart
 	
-	} // end while
+	gameEnd(isGameWon){
 
-}; // end shufflePictures
+		this.gameBoard.addClass('no_game_board');
+		this.btnPlay.text('Click to Play Game');
 
-MatchGame.prototype.addPictures = function(thePictures){
+		if(isGameWon === false){
 
-	this.boxes.each(function(i){
-		
-		// create image
-		var $theImg = $('<img>').attr('src', thePictures[i]);
-		
-		// get the box that we will add the image to		  
-		var $theBox = $(this).find('.box-back');	
-		// add image to the box	
-		$theImg.appendTo($theBox);								  				  
-		
-	}); // end .each()
+			this.boxes.removeClass('flipped')
+					  .removeClass('matched');
 
-}; // end addPictures
+		}
 
-MatchGame.prototype.destroyPictures = function(){
-
-	this.pictures.length = 0;
-	$('.box-back img').remove();
-
-}; // end destroyPictures
-
-MatchGame.prototype.gameTurn = function(box){
-
-	// Get a reference to this...
-	var that = this;
-	
-	if(box.hasClass('flipped')){
-		
-		return;
-	
-	} // end if box has class flipped or matched
-
-	this.clickCounter++;
-
-	if(this.clickCounter === 1){
-
-		box.addClass('flipped');
-		this.firstFlippedBox = box;
-		this.firstFlippedPicture = box.find('.box-back img').attr('src');
-		
-		return;
-
-	} // end if clickCounter === 1
-
-	// CounterClicks equals 2 -> look for match
-	if(this.clickCounter === 2){
-
-		box.addClass('flipped');
-		this.secondFlippedBox = box;
-		this.secondFlippedPicture = box.find('.box-back img').attr('src');
-
-	}	
-
-	if(this.firstFlippedPicture === this.secondFlippedPicture){
-		// they match
-		this.firstFlippedBox.addClass('matched');
-		this.secondFlippedBox.addClass('matched');
 		this.clickCounter = 0;
-		this.updateTurns();
-		this.updateMatch();
-		
-		box.one('transitionend', function(){
+		this.turns = 0;
+		this.matches = 0;
+		this.matchedOut.text(0);
+		this.turnsOut.text(0);
+		this.destroyPictures();
 
-			if(that.matches === that.halfNumBoxes){
-				that.gameWon();
-				return;
-			}
+	} // end gameEnd
+	
+	createPictures(){
 
-		});		
+		for(let i = 1; i <= this.halfNumBoxes; i++){
+			if(i < 10){			
+				// for images 01 - 09
+				// .push() inserts an image into the array..
+				this.pictures.push(this.picFilename + '0' + i + '.jpg');
+				this.pictures.push(this.picFilename + '0' + i + '.jpg');
 
-	}else{
-		// they do not match
-		setTimeout(function(){
-			that.updateTurns();
-			that.firstFlippedBox.removeClass('flipped');
-			that.secondFlippedBox.removeClass('flipped');
-			that.clickCounter = 0;
-		}, 750);
-	} // end if images match or not...	
+			}else{
+				// for images 10 - 18
+				this.pictures.push(this.picFilename + i + '.jpg');
+				this.pictures.push(this.picFilename + i + '.jpg');
 
-}; // end gameTurn
+			} // end if (i < 10)	
+		} // end for loop
 
-MatchGame.prototype.updateTurns = function(){
+	} // end createPictures
+	
+	shufflePictures(){
 
-	this.turns++;
-	this.turnsOut.text(this.turns);
+		let counter = this.pictures.length;
+		let temp; 
+		let i;
 
-}; // end updateTurns
+		// While there are elements in the array
+		while (counter > 0) {
 
-MatchGame.prototype.updateMatch = function(){
+			// Pick a random index
+			i = Math.floor(Math.random() * counter);
 
-	this.matches++;
-	this.matchedOut.text(this.matches);
+			// Decrease counter by 1
+			counter--;
 
-}; // end updateMatch
+			// And swap the last element with it
+			temp = this.pictures[counter];
+			this.pictures[counter] = this.pictures[i];
+			this.pictures[i] = temp;
 
-MatchGame.prototype.gameWon = function(){
+		} // end while
 
-	var answer = confirm('You Matched them all...Click OK to play again?');
+	} // end shufflePictures
 
-	if(answer === true){
+	addPictures(thePictures){
 
-		var that = this;
-		
-		this.boxes.removeClass('flipped')
-		          .removeClass('matched')
-				  .one('transitionend', function(){
-					  that.gameEnd(true);
-					  that.gameStart();
-					  that.boxes.off('transitionend');
-				   });
-		
-	} // end if answer is true
+		this.boxes.each(function(i){
 
-}; // end gameWon
+			// create image
+			const $theImg = $('<img>').attr('src', thePictures[i]);
 
+			// get the box that we will add the image to		  
+			const $theBox = $(this).find('.box-back');	
+			// add image to the box	
+			$theImg.appendTo($theBox);								  				  
 
-var mg = new MatchGame($('.game_board'), 
-	                   $('.col'), 
-	                   $('.col').length, 
-	                   $('.output_turns p'), 
-	                   $('.output_matched p'),
-	                   $('.btn_play_and_reset'),
-	                   'images/animal_image_');
+		}); // end .each()
+
+	} // end addPictures
+	
+	destroyPictures(){
+
+		this.pictures.length = 0;
+		$('.box-back img').remove();
+
+	} // end destroyPictures
+	
+	gameTurn(box){
+
+		// Get a reference to this...
+		const that = this;
+
+		if(box.hasClass('flipped')){
+
+			return;
+
+		} // end if box has class flipped or matched
+
+		this.clickCounter++;
+
+		if(this.clickCounter === 1){
+
+			box.addClass('flipped');
+			this.firstFlippedBox = box;
+			this.firstFlippedPicture = box.find('.box-back img').attr('src');
+
+			return;
+
+		} // end if clickCounter === 1
+
+		// CounterClicks equals 2 -> look for match
+		if(this.clickCounter === 2){
+
+			box.addClass('flipped');
+			this.secondFlippedBox = box;
+			this.secondFlippedPicture = box.find('.box-back img').attr('src');
+
+		}	
+
+		if(this.firstFlippedPicture === this.secondFlippedPicture){
+			// they match
+			this.firstFlippedBox.addClass('matched');
+			this.secondFlippedBox.addClass('matched');
+			this.clickCounter = 0;
+			this.updateTurns();
+			this.updateMatch();
+
+			box.one('transitionend', function(){
+
+				if(that.matches === that.halfNumBoxes){
+					that.gameWon();
+					return;
+				}
+
+			});		
+
+		}else{
+			// they do not match
+			setTimeout(function(){
+				that.updateTurns();
+				that.firstFlippedBox.removeClass('flipped');
+				that.secondFlippedBox.removeClass('flipped');
+				that.clickCounter = 0;
+			}, 750);
+			
+		} // end if images match or not...	
+
+	} // end gameTurn
+
+	updateTurns(){
+
+		this.turns++;
+		this.turnsOut.text(this.turns);
+
+	} // end updateTurns
+	
+	updateMatch(){
+
+		this.matches++;
+		this.matchedOut.text(this.matches);
+
+	} // end updateMatch
+	
+	gameWon(){
+
+		const answer = confirm('You Matched them all...Click OK to play again?');
+
+		if(answer === true){
+
+			const that = this;
+
+			this.boxes.removeClass('flipped')
+					  .removeClass('matched')
+					  .one('transitionend', function(){
+						  that.gameEnd(true);
+						  that.gameStart();
+						  that.boxes.off('transitionend');
+					   });
+
+		} // end if answer is true
+
+	} // end gameWon
+	
+} // end Game Class
+
+// Game Functions
+const mg = new MatchGame($('.game_board'), 
+	                     $('.col'), 
+	                     $('.col').length, 
+	                     $('.output_turns p'), 
+	                     $('.output_matched p'),
+	                     $('.btn_play_and_reset'),
+	                     'images/animal_image_');
 
 // Cheat code
 // mg.matches = mg.halfNumBoxes - 1;
